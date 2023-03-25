a0ed9e1b5ee3fca1c6f62088ee1573a29d777644
Dr. Mary Lebens <52386621+marylebens@users.noreply.github.com>
2023-03-25 13:29:28 -0500

    Create Summit.java

---
+++ src/summit/Summit.java
@@ -0,0 +1,15 @@
+package summit;
+    
+/**
+ * Instructions: 
+ * This is a template for Summit students to create a Java app.
+ * Please refactor the class name before you start coding.
+ * Change the class name and the Java file name to the name of your app.
+ * @author Dr. Mary Lebens
+ */
+public class Summit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World from Summit Academy OIC!");
+    } //end main method
+} //end class
